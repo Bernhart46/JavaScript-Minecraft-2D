63bf64a3717394bd79d6b2fa55ec3038ef91000b
Bernhart46 <hegyiferenc@proton.me>
2024-05-31 05:07:08 +0200

    Refactor things, only top collision works

---
+++ object.js
@@ -0,0 +1,58 @@
+import { Vector2D } from "./vector2d.js";
+let groundLevel = window.innerHeight - 50;
+const playerHeight = 100;
+
+export class Object {
+  constructor({
+    id = Date.now(),
+    color = "black",
+    x = 0,
+    y = 0,
+    w = 50,
+    h = 50,
+  }) {
+    this.id = id;
+    this.color = color;
+    this.width = w;
+    this.height = h;
+    this.pos = new Vector2D(x, y);
+  }
+}
+
+export const player = new Object({
+  id: 0,
+  color: "blue",
+  x: window.innerWidth / 3,
+  y: groundLevel - playerHeight - 350,
+  w: 50,
+  h: playerHeight,
+});
+
+export const objects = [
+  new Object({
+    id: 1,
+    color: "red",
+    // x: 150,
+    x: 400,
+    y: groundLevel - 50,
+    w: 50,
+    h: 50,
+  }),
+  new Object({
+    id: 3,
+    color: "yellow",
+    // x: 150,
+    x: 250,
+    y: groundLevel - 170,
+    w: 50,
+    h: 50,
+  }),
+  new Object({
+    id: 2,
+    color: "green",
+    x: 0,
+    y: groundLevel,
+    w: window.innerWidth,
+    h: 50,
+  }),
+];
--- script.js
+++ script.js
@@ -1,150 +1,66 @@
 "use script";
+import { objects, player } from "./object.js";
+import { Vector2D } from "./vector2d.js";
 
 const canvas = document.querySelector("#myCanvas");
-const ctx = canvas.getContext("2d");
+export const ctx = canvas.getContext("2d");
 const fps = 30;
 //Custom time
 const tickSpeed = 40;
+const keyPressed = {};
+let zoom = 1;
 
+export let groundLevel = window.innerHeight - 50;
 const gravity = 3;
 const moveSpeed = 10;
-let groundLevel = window.innerHeight - 50;
-const playerHeight = 100;
+export const playerHeight = 100;
 
 canvas.setAttribute("width", window.innerWidth);
 canvas.setAttribute("height", window.innerHeight);
 
-class Vector2D {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
 const CAMERA = new Vector2D(0, 0);
 const velocity = new Vector2D(0, 0);
-
-class Object {
-  constructor({
-    id = Date.now(),
-    color = "black",
-    x = 0,
-    y = 0,
-    w = 50,
-    h = 50,
-  }) {
-    this.id = id;
-    this.color = color;
-    this.pos = new Vector2D(x, y);
-    this.width = w;
-    this.height = h;
-  }
-}
-const player = new Object({
-  id: 0,
-  color: "blue",
-  x: window.innerWidth / 2,
-  y: groundLevel - playerHeight - 200,
-  w: 50,
-  h: playerHeight,
-});
-const objects = [
-  player,
-  new Object({
-    id: 1,
-    color: "red",
-    // x: 150,
-    x: window.innerWidth / 2,
-    y: groundLevel - 50,
-    w: 50,
-    h: 50,
-  }),
-  new Object({
-    id: 3,
-    color: "yellow",
-    // x: 150,
-    x: 250,
-    y: groundLevel - 100,
-    w: 50,
-    h: 50,
-  }),
-  new Object({
-    id: 2,
-    color: "green",
-    x: 0,
-    y: groundLevel,
-    w: window.innerWidth,
-    h: 50,
-  }),
-];
-
-//ONLY FOR DRAWING NOT CALCULING!!! CALCULATIONS ARE FOR THE TICKS TO DECIDE NOT THE FPS
-function draw() {
-  for (let object of objects) {
-    const { color, pos, width, height } = object;
-    drawObject(color, pos.x, pos.y, width, height);
-  }
-
-  //COORDS
-  ctx.fillText(`P_X: ${player.pos.x}`, 10, 20);
-  ctx.fillText(`P_Y: ${-player.pos.y + groundLevel - playerHeight}`, 10, 40);
-  ctx.fillText(`C_X: ${CAMERA.x}`, 10, 60);
-  ctx.fillText(`C_Y: ${CAMERA.y}`, 10, 80);
-  ctx.fillText(`Controls: A,S,D + W or Space`, 10, 100);
-}
-
-function drawObject(style, x, y, w, h) {
-  ctx.fillStyle = style;
-  ctx.fillRect(x - CAMERA.x, y, w, h);
-}
 
 //Reason why it's separate from draw: Because in here, it calculates without being affected by the fps!
 function calculate() {
   //COLLISION
-  let isStopped = [];
-  let l = [],
-    r = [];
-  let y;
-  let bottomBlock;
-  for (let i = 1; i < objects.length; i++) {
-    const object = objects[i];
-    //BOTTOM
-    bottomBlock =
-      player.pos.y < object.pos.y - playerHeight - velocity.y - gravity - 1;
-    const rightBlock = player.pos.x > object.pos.x + object.width;
-    const leftBlock = player.pos.x + player.width < object.pos.x;
-    if (bottomBlock || rightBlock || leftBlock) {
-      isStopped.push(false);
-    } else {
-      isStopped.push(true);
-      y = object.pos.y;
+  let stopTopY;
+  let stopTopX;
+  let stopLeftX;
+  const playerBottom = player.pos.y + player.height + velocity.y + 1;
+  const playerTop = player.pos.y + velocity.y + 1;
+  for (let obj of objects) {
+    const isCollidedY =
+      obj.pos.y <= playerBottom && playerTop <= obj.pos.y + obj.height;
+    const isCollidedTopX =
+      player.pos.x + player.width >= obj.pos.x &&
+      obj.pos.x + obj.width >= player.pos.x;
+
+    if (isCollidedY && isCollidedTopX) {
+      stopTopY = obj.pos.y;
+      stopTopX = obj.pos.x;
     }
-    r.push(object.pos.x);
-    l.push(object.pos.x + object.width);
   }
-  let isFall = !isStopped.includes(true) ? true : false;
-  r = r.sort((x, y) => x - y).filter((x) => x > player.pos.x);
-  const closestR = r.length === 0 ? false : Math.min(...r);
-  l.sort((x, y) => x - y);
-  l = l.sort((x, y) => x - y).filter((x) => x < player.pos.x + player.width);
-  const closestL = l.length === 0 ? false : Math.max(...l);
-
-  const canMoveRight =
-    closestR === false
-      ? true
-      : player.pos.x + player.width + velocity.x + 1 < closestR;
-  const canMoveLeft =
-    closestL === false ? true : player.pos.x + velocity.x - 1 > closestL;
-
-  if (isFall) {
-    velocity.y = velocity.y + gravity;
+  console.log(stopLeftX);
+  if (!stopTopY && !stopTopX) {
+    velocity.y += gravity;
   } else {
-    velocity.y = 0;
-    player.pos.y = y - playerHeight - 1;
+    if (!keyPressed["Space"]) {
+      velocity.y = 0;
+    }
+    player.pos.y = stopTopY - player.height - velocity.y - 1;
   }
+
   //REGISTERING MOVEMENT
-  player.pos.x = player.pos.x + velocity.x;
-  player.pos.y = player.pos.y + velocity.y;
-  CAMERA.x = -(window.innerWidth / 2 - player.pos.x - 25);
+  player.pos.x += velocity.x;
+  player.pos.y += velocity.y;
+  CAMERA.x = -(window.innerWidth / zoom / 2 - player.pos.x - player.width / 2);
+  CAMERA.y = -(
+    window.innerHeight / zoom / 2 -
+    player.pos.y -
+    player.height / 2
+  );
+
   //SLOWING DOWN X (NOT TO GO FOREVER)
   if (velocity.x < 0) {
     velocity.x++;
@@ -155,29 +71,22 @@ function calculate() {
 
   //KEY ACTIONS HERE!!!
   if (keyPressed["Space"] || keyPressed["KeyW"]) {
-    if (!isFall) {
-      velocity.y = velocity.y - 30;
+    if (!isNaN(stopTopY)) {
+      velocity.y -= 30;
     }
   }
-
-  // console.log(canMoveRight);
-  if (keyPressed["KeyD"] && (canMoveRight || bottomBlock)) {
-    velocity.x = moveSpeed;
+  if (keyPressed["KeyD"]) {
+    if (true) {
+      velocity.x = moveSpeed;
+    } else {
+      velocity.x = 0;
+      player.pos.x = stopLeftX - player.width - velocity.x;
+    }
   }
-  if (keyPressed["KeyA"] && (canMoveLeft || bottomBlock)) {
+  if (keyPressed["KeyA"]) {
     velocity.x = -moveSpeed;
   }
-  if (!canMoveRight && !bottomBlock && velocity.x > 0) {
-    velocity.x = 0;
-    player.pos.x = closestR - player.width - 1;
-  }
-  if (!canMoveLeft && !bottomBlock && velocity.x < 0) {
-    velocity.x = 0;
-    player.pos.x = closestL + 1;
-  }
 }
-
-const keyPressed = {};
 
 //KEY EVENTS ONLY FOR REGISTERING, NOT ACTIVATING ACTIONS!!!
 window.addEventListener("keydown", (e) => {
@@ -192,8 +101,58 @@ window.addEventListener("keyup", (e) => {
 window.addEventListener("resize", () => {
   canvas.setAttribute("width", window.innerWidth);
   canvas.setAttribute("height", window.innerHeight);
+
   groundLevel = window.innerHeight - 50;
 });
+
+window.addEventListener("wheel", (e) => {
+  if (e.wheelDelta < 0) {
+    if (zoom > 0.2) {
+      zoom -= 0.1;
+    }
+  } else {
+    zoom += 0.1;
+  }
+});
+
+function gameTime() {
+  setInterval(calculate, 1000 / tickSpeed);
+}
+
+gameTime();
+
+//ONLY FOR DRAWING NOT CALCULING!!! CALCULATIONS ARE FOR THE TICKS TO DECIDE NOT THE FPS
+function draw() {
+  drawObject(
+    player.color,
+    player.pos.x,
+    player.pos.y,
+    player.width,
+    player.height
+  );
+  for (let object of objects) {
+    const { color, pos, width, height } = object;
+    drawObject(color, pos.x, pos.y, width, height);
+  }
+
+  //COORDS
+  ctx.fillText(`P_X: ${player.pos.x}`, 10, 20);
+  ctx.fillText(`P_Y: ${-player.pos.y + groundLevel - playerHeight}`, 10, 40);
+  ctx.fillText(`C_X: ${CAMERA.x}`, 10, 60);
+  ctx.fillText(`C_Y: ${CAMERA.y}`, 10, 80);
+  ctx.fillText(`Zoom: ${zoom.toFixed(1)}`, 10, 100);
+  ctx.fillText(`Controls: A,S,D + W or Space`, 10, 120);
+}
+
+function drawObject(style, x, y, w, h) {
+  ctx.fillStyle = style;
+  ctx.fillRect(
+    x * zoom - CAMERA.x * zoom,
+    y * zoom - CAMERA.y * zoom,
+    w * zoom,
+    h * zoom
+  );
+}
 
 //ANIMATE (fps)
 function animate() {
@@ -205,9 +164,3 @@ function animate() {
 }
 
 animate();
-
-function gameTime() {
-  setInterval(calculate, 1000 / tickSpeed);
-}
-
-gameTime();
